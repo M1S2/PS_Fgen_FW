bdef9596c669f2a48243c89d13221f943ffb5692
Markus Scheich <markus.scheich@gmx.de>
2020-08-30 15:43:15 +0200

    u8glib integrated

--- PowerSupplySigGen/main.cpp
+++ PowerSupplySigGen/main.cpp
@@ -8,34 +8,46 @@
 #include <avr/io.h>
 #include "Spi/spi.h"
 #include "Pins/Pins.h"
-#include "GLCD/VLGEM1277.h"
+#include "GLCD/u8g.h"
 
 #include <avr/interrupt.h>
+
+u8g_t u8g;
+
+void draw(void)
+{
+	u8g_SetDefaultForegroundColor(&u8g);
+	u8g_SetFont(&u8g, u8g_font_6x10);
+	u8g_DrawStr(&u8g, 7, 15, "Hello World!");
+	u8g_SetFont(&u8g, u8g_font_freedoomr10r);
+	u8g_DrawStr(&u8g, 7, 40, "ABC");
+	u8g_SetFont(&u8g, u8g_font_fub49n);
+	u8g_DrawStr(&u8g, 70, 55, "0");
+	u8g_SetDefaultMidColor(&u8g);
+	u8g_DrawLine(&u8g, 5, 5, 235, 5);
+	u8g_DrawLine(&u8g, 5, 5, 5, 60);
+	u8g_DrawLine(&u8g, 235, 5, 235, 60);
+	u8g_DrawLine(&u8g, 5, 60, 235, 60);
+	u8g_DrawCircle(&u8g, 200, 33, 25, U8G_DRAW_ALL);
+}
 
 int main(void)
 {
 	cli();
 	Pins_Init();
 	SPI_Init();
-	lcd_init();	
 	sei();
 	
-	lcd_clear();
-
-	// test
-	lcd_clear();
-	for (uint8_t y=0;y<8;y++) 
+	u8g_InitSPI(&u8g, &u8g_dev_s1d15721_hw_spi, PN(1, 7), PN(1, 5), PN(1, 1), PN(1, 0), U8G_PIN_NONE);
+	
+	for(;;)
 	{
-		for (uint8_t i=0;i<40;i++) 
+		u8g_FirstPage(&u8g);
+		do
 		{
-			lcd_char(1+y%3,i,y,0x20+19+i-y);
-		}
+			draw();
+		} while ( u8g_NextPage(&u8g) );
+		u8g_Delay(100);
 	}
 	
-		
-    while (1) 
-    {
-
-    }
 }
-

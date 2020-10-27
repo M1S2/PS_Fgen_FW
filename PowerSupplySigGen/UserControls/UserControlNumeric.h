10e1ca408edea76a9da7702af4a37fd896e24139
Markus Scheich <markus.scheich@gmx.de>
2020-10-27 11:34:28 +0100

    UserControlNumeric improved, Debug code not removed yet

--- PowerSupplySigGen/UserControls/UserControlNumeric.h
+++ PowerSupplySigGen/UserControls/UserControlNumeric.h
@@ -23,6 +23,13 @@ class UserControlNumeric : public UserControlBase<T>
 		int8_t _unitPrefixPower;		// Current display prefix power (m = -3, k = 3, M = 6)
 		float _displayValue;			// Value that is displayed by Draw(). E.g. 999.760 if control value is 999760 Hz.
 
+#if 0
+		/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+		float DebugDisplayNum;
+		char DebugDisplayStr[2];
+		/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+#endif
+
 		T coerceValue(T value)
 		{
         	if(value > _maxValue) { value = _maxValue; }
@@ -30,13 +37,19 @@ class UserControlNumeric : public UserControlBase<T>
         	return value;
 		}
 		
+		/*void coerceDisplayValue()
+		{
+			if(_displayValue > 999.999) { _displayValue = 999.999; }
+			else if(_displayValue < -999.999) { _displayValue = -999.999; }
+		}*/
+		
 		void calculateDisplayValue()
 		{
         	_displayValue = (float)*this->_controlValue;
 			if(_displayValue != 0)
 			{
         		_unitPrefixPower = _valueStoreUnitPower;
-        		while(abs(_displayValue) >= 1000)
+        		while(fabs(_displayValue) >= 1000)
         		{
         			_unitPrefixPower += 3;
         			_displayValue /= 1000;
@@ -55,11 +68,31 @@ class UserControlNumeric : public UserControlBase<T>
 		
 		// https://www.quora.com/How-can-you-mathematically-extract-a-single-digit-from-a-number
 		uint8_t extractDigit(float number, int8_t position)
-		{	
+		{
 			float divisor = pow(10, position);
-			uint32_t truncated = uint32_t(fabs(number) / divisor);
+			uint32_t truncated = uint32_t((fabs(number) / divisor) + 0.1f);		// +0.1f not really clean workaround. Is there some rounding problem?
+			//DebugDisplayNum = (fabs(number) / divisor) + 0.1f;
 			return truncated % 10;
 		}
+		
+#if 0
+		// !!!!!!!!!!! Not working yet !!!!!!!!!!!!!!!!!!!!
+		uint8_t extractDigitViaString(float number, int8_t position)
+		{
+			char stringBuffer[9];
+			dtostrf(number, 8, 3, stringBuffer);
+			for(int i = 1; i < 8; i++)
+			{
+				if (stringBuffer[i] == ' ') { stringBuffer[i] = '0'; }
+			}
+			//sprintf(stringBuffer, "%07.3f", fabs(number));
+			char digitChar = stringBuffer[-position + 2 + (position < 0 ? 1 : 0)];
+			//DebugDisplayStr[0] = digitChar;
+			//DebugDisplayStr[1] = '\0';
+			//DebugDisplayNum = -position + 2 + (position < 0 ? 1 : 0);
+			return digitChar - '0';     // Remove character offset from digit
+		}
+#endif
 		
     public:
         UserControlNumeric(uint8_t locx, uint8_t locy, T* controlValue, char* baseUnit, int8_t valueStoreUnitPower, T minValue, T maxValue) : UserControlBase<T>(locx, locy, controlValue)
@@ -71,24 +104,36 @@ class UserControlNumeric : public UserControlBase<T>
              _currentDigitPosition = 0;
              _unitPrefixPower = 0;
              calculateDisplayValue();
+			 
+			 
+			 //_displayValue = 123.456;
+			 //DebugDisplayNum = 3;
         }
         
         void KeyInput(Keys_t key)
         {
             if(this->IsActive && key != KEYNONE)
             {
-                if(key == KEYLEFT && _currentDigitPosition < 2) { _currentDigitPosition++; }		// Move cursor left
-        		else if(key == KEYRIGHT && _currentDigitPosition > -3) { _currentDigitPosition--; }	// Move cursor right
+                if(key == KEYLEFT)			// Move cursor left
+				{
+					 if(_currentDigitPosition < 2) { _currentDigitPosition++; } 
+				}		
+        		else if(key == KEYRIGHT)	// Move cursor right
+				{
+					if(_currentDigitPosition > -3) { _currentDigitPosition--; } 
+				}	
         		else if(key == KEYMILLI) { /*this->IsActive = false;*/ _unitPrefixPower = -3; recalculateControlValue(); calculateDisplayValue(); }
         		else if(key == KEYKILO) { /*this->IsActive = false;*/ _unitPrefixPower = 3; recalculateControlValue(); calculateDisplayValue(); }
         		else if(key == KEYMINUS) { _displayValue *= -1; }
-				else if(key == KEYCOMMA) { _displayValue /= pow(10, (_currentDigitPosition + 1)); _currentDigitPosition = -1; }
+				else if(key == KEYCOMMA) { _displayValue = _displayValue / pow(10, (_currentDigitPosition + 1)); /*coerceDisplayValue();*/ _currentDigitPosition = -1; }
 				else
 				{
 					uint8_t keyNum = KeyPad_GetKeyNumInt(key);
-					uint8_t digit = extractDigit(_displayValue, _currentDigitPosition);
+					uint8_t digit = extractDigit(_displayValue, _currentDigitPosition);					
+					//uint8_t digit = extractDigitViaString(_displayValue, _currentDigitPosition);
 					float multiplicator = pow(10, _currentDigitPosition);
 					_displayValue = _displayValue - (digit * multiplicator) + (keyNum * multiplicator);
+					//coerceDisplayValue();
 					if(_currentDigitPosition > -3) { _currentDigitPosition--; }	// Move cursor right
 				}
             }
@@ -115,6 +160,7 @@ class UserControlNumeric : public UserControlBase<T>
 				}
 				//*this->_controlValue = coerceValue(value);
 				//calculateDisplayValue();
+				//coerceDisplayValue();
 			}
 		}
 		
@@ -135,9 +181,20 @@ class UserControlNumeric : public UserControlBase<T>
 			if(_displayValue < 0) { stringBuffer[0] = '-'; }
 			u8g_DrawStr(u8g, this->_locX + 3 - (_displayValue < 0 ? 1 : 0), this->_locY + 3 + CONTROLS_FONT_HEIGHT, stringBuffer);
 			
-			u8g_SetDefaultBackgroundColor(u8g);
+			//u8g_SetDefaultBackgroundColor(u8g);
+			
 			u8g_uint_t cursorXpos = this->_locX + (-_currentDigitPosition + 3) * 6 + (_currentDigitPosition < 0 ? 3 : 0);
 			u8g_DrawHLine(u8g, cursorXpos, this->_locY + CONTROLS_FONT_HEIGHT + 4, 5);
+			
+			
+#if 0
+			u8g_SetDefaultForegroundColor(u8g);
+			char debugStringBuffer[10];
+			dtostrf(DebugDisplayNum, 10, 5, debugStringBuffer);
+			u8g_DrawStr(u8g, this->_locX + 3, this->_locY + 10 + 2 * CONTROLS_FONT_HEIGHT, debugStringBuffer);
+			
+			u8g_DrawStr(u8g, this->_locX + 3, this->_locY + 15 + 3 * CONTROLS_FONT_HEIGHT, DebugDisplayStr);
+#endif
 		}
 };
 

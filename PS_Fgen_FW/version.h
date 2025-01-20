9c41c4dc6bb1e594be0c927a3f84e53057acd148
Markus Scheich <markus.scheich@gmx.de>
2025-01-20 18:39:44 +0100

    Created release v2.0.0

--- PS_Fgen_FW/version.h
+++ PS_Fgen_FW/version.h
@@ -5,6 +5,6 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSION		"[NO_RELEASE]"			/**< Software version. Format e.g. "v1.0.0". This should be incremented with every release. */
+#define VERSION		"v2.0.0"			/**< Software version. Format e.g. "v1.0.0". This should be incremented with every release. */
 
 #endif /* VERSION_H_ */
